b734fd90b6a8e612eadb9b50c56f0299671fc153
Vanya <differentstuff777@gmail.com>
2019-04-10 17:58:44 +0300

    working conf

--- util/io/p1.c
+++ util/io/p1.c
@@ -20,9 +20,8 @@ ssize_t getline_no_nl(char **lineptr, size_t *n, FILE *stream){
  return ret;
 }
 
-int conf_alloc(struct conf_file* conf, FILE* fp){
-  conf = malloc(sizeof(struct conf_file));
-  conf->reaper = NULL;
+int conf_alloc(struct conf_file** conf, FILE* fp){
+  *conf = malloc(sizeof(struct conf_file));
 
   char* buff = NULL;
   size_t len = 0;
@@ -30,36 +29,37 @@ int conf_alloc(struct conf_file* conf, FILE* fp){
   int node_cnt = atoi(buff);
   free(buff);
 
-  conf->node_cnt = node_cnt;
-  conf->entries = malloc(sizeof(struct node_entry) * node_cnt);
-  for (int i = 0; i < NODE_ENTRY_FIELDS * node_cnt; i++){
-    conf->entries[i].ip_addr = NULL;
-    conf->entries[i].hostname = NULL;
-    conf->entries[i].task = NULL;
+  (*conf)->node_cnt = node_cnt;
+  (*conf)->entries = malloc(sizeof(struct node_entry) * node_cnt);
+  for (int i = 0; i < node_cnt; i++){
+    (*conf)->entries[i].ip_addr = NULL;
+    (*conf)->entries[i].hostname = NULL;
+    (*conf)->entries[i].task = NULL;
 
     len = 0;
-    getline_no_nl(&(conf->entries[i].ip_addr), &len, fp);
+    getline_no_nl(&((*conf)->entries[i].ip_addr), &len, fp);
 
     len = 0;
-    getline_no_nl(&(conf->entries[i].hostname), &len, fp);
+    getline_no_nl(&((*conf)->entries[i].hostname), &len, fp);
 
     len = 0;
-    getline_no_nl(&(conf->entries[i].task), &len, fp);
+    getline_no_nl(&((*conf)->entries[i].task), &len, fp);
   }
 
   len = 0;
-  conf->reaper = NULL;
-  getline_no_nl(&(conf->reaper), &len, fp);
+  (*conf)->reaper = NULL;
+  getline_no_nl(&((*conf)->reaper), &len, fp);
+  printf("kek\n");
 }
 
 void print_conf_file(struct conf_file* conf){
-  printf("%s\n", conf->reaper);
   printf("%d\n", conf->node_cnt);
   for (int i = 0; i < conf->node_cnt; i++){
     printf("%s\n", conf->entries[i].ip_addr);
     printf("%s\n", conf->entries[i].hostname);
     printf("%s\n", conf->entries[i].task);
   }
+  printf("%s\n", conf->reaper);
 }
 int main(int argc, char* argv[]){
 
@@ -74,9 +74,12 @@ int main(int argc, char* argv[]){
   }
   */
 
+  printf("(%s)\n", argv[1]);
   FILE *fp = fopen(argv[1], "r");
+  if (fp == NULL)
+    printf("wat\n");
   struct conf_file* conf = NULL;
-  conf_alloc(conf, fp);
+  conf_alloc(&conf, fp);
   print_conf_file(conf);
 }
 
@@ -106,4 +109,4 @@ int main(int argc, char* argv[]){
 
 
 
-//
+//gcc -g -O0 p1.c -o p1
